2e1e3994fae0fa9659a4799486f89b7982441b38
bekahjane <bekahjane88@icloud.com>
2022-02-20 19:54:09 -0700

    Showing the current temperature

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "7408fdf253ddebead7e68be64b518024";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Buckeye&units=metric&appid=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemperature);
